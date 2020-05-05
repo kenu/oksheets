cb654a55cd19983a0f470a4f669cc9de75f33b8c
kenu <kenu.heo@gmail.com>
2020-05-06 03:00:06 +0900

    Service Account Working

--- google-sp/googlesheets.js
+++ google-sp/googlesheets.js
@@ -6,24 +6,27 @@ const { GoogleSpreadsheet } = require('google-spreadsheet');
 // spreadsheet key is the long id in the sheets URL
 const doc = new GoogleSpreadsheet('<the sheet ID from the url>');
 
-// use service account creds
-await doc.useServiceAccountAuth({
-  client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-  private_key: process.env.GOOGLE_PRIVATE_KEY,
-});
-// OR load directly from json file if not in secure environment
-await doc.useServiceAccountAuth(require('./creds-from-google.json'));
-// OR use API key -- only for read-only access to public sheets
-doc.useApiKey('YOUR-API-KEY');
+async function doiT() {
+  // use service account creds
+  // simply go in your browser to the Google sheet you want to interact with
+  // go to SHARE on the top right of your screen
+  // go to advanced settings and share it with email address of your service account
+  await doc.useServiceAccountAuth({
+    client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY,
+  });
+  await doc.loadInfo(); // loads document properties and worksheets
+  console.log(doc.title);
+  await doc.updateProperties({ title: 'renamed doc' });
 
-await doc.loadInfo(); // loads document properties and worksheets
-console.log(doc.title);
-await doc.updateProperties({ title: 'renamed doc' });
+  const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id]
+  console.log(sheet.title);
+  console.log(sheet.rowCount);
 
-const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id]
-console.log(sheet.title);
-console.log(sheet.rowCount);
+  // adding / removing sheets
+  const newSheet = await doc.addSheet({ title: 'hot new sheet2' });
+  await newSheet.delete();
+}
 
-// adding / removing sheets
-const newSheet = await doc.addSheet({ title: 'hot new sheet!' });
-await newSheet.delete();
+doiT();
+
